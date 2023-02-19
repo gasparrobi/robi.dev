9fe42c3164be014239575e2e635c53886ec16c78
gasparrobi <gasparrobi@gmail.com>
2023-02-19 20:35:21 +0100

    add techfizu and inflacio pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          defer
+          data-website-id={process.env.STATS_WEBSITE_ID}
+          src={process.env.STATS_URL}
+        ></script>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
@@ -15,12 +16,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-col justify-between min-h-screen p-4 md:p-24">
-        <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <a
+            href="https://techfizu.robi.dev"
+            className={styles.card}
+            target="_blank"
+            rel="noopener"
+          >
+            <h2 className={inter.className}>
+              techfizu.robi.dev <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Stats about software dev salaries in Hungary
+            </p>
+          </a>
+
+          <a
+            href="https://inflacio.robi.dev"
+            className={styles.card}
+            target="_blank"
+            rel="noopener"
+          >
+            <h2 className={inter.className}>
+              inflacio.robi.dev <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Calculate the depreciation of your money
+            </p>
+          </a>
+
           <a
             href="https://frontendfizu.robi.dev"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener"
           >
             <h2 className={inter.className}>
               frontendfizu.robi.dev <span>-&gt;</span>
@@ -34,7 +63,7 @@ export default function Home() {
             href="https://budget.robi.dev"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener"
           >
             <h2 className={inter.className}>
               budget.robi.dev <span>-&gt;</span>
